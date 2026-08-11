6ee32562b3ceb582bddd8e36b3967cdfc83e94fb
yohansaji <yohansaji.hgpf@gmail.com>
2026-08-11 16:53:04 +0530

    Create day_1.py

---
+++ day_1.py
@@ -0,0 +1,8 @@
+#Just a beginner program to perform basic arithmetic operations
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+
+print("Sum = ",a+b)
+print("Sub = ",a-b)
+print("Mul = ",a*b)
+print("Div = ",a/b)
