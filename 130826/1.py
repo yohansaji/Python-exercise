afc9f5a69ba603aa790dc3248bd34cdd574af4b5
yohansaji <yohansaji.hgpf@gmail.com>
2026-08-14 10:58:50 +0530

    .

---
+++ 130826/1.py
@@ -0,0 +1,18 @@
+a=["Subin","Sandra","Yohan","Deborah"]
+a.insert(0,"Saji")
+a.insert(1,"Bindu")
+a.append("Pippy")
+#a.remove("yohan")
+#a.pop(4)
+
+
+#print(a)
+#print(len(a))
+
+
+if "Yohan" in a:
+    print("Yohan is present in the list")
+
+print("Pippy" in a)
+
+print(a[0:0:-1])
